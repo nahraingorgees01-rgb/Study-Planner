ca2ca5c4fe1177ae70c87a602122d74c1853bf9f
nahrain33 <nahraingorgees01@gmail.com>
2025-12-16 19:24:19 -0800

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/planner")
+def planner():
+    return render_template("planner.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT if available
-    app.run(host="0.0.0.0", port=port, debug=True)  # debug=True is helpful locally
+    app.run(debug=True)
